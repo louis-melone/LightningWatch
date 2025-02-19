0a471686b35ca263a5706335354d4d7316c67b10
louis-melone <114934187+louis-melone@users.noreply.github.com>
2025-02-19 15:44:25 -0500

    add project

---
+++ LightningWatch/LightningWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightningWatchApp.swift
+//  LightningWatch
+//
+//  Created by Louis Melone on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LightningWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
