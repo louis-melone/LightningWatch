e7c9df07279487506a2006c6f551882d21e7ea4f
louis-melone <114934187+louis-melone@users.noreply.github.com>
2025-02-19 18:15:30 -0500

    city / country business logic

--- LightningWatch/ViewModel/NodeViewModel.swift
+++ LightningWatch/ViewModel/NodeViewModel.swift
@@ -8,4 +8,14 @@
 struct NodeViewModel: Identifiable {
     let node: LightningNode
     var id: String { node.id }
+    
+    var cityName: String? {
+        guard let city = node.city else { return nil }
+        return city.ptBR ?? city.en
+    }
+    
+    var countryName: String? {
+        guard let country = node.country else { return nil }
+        return country.ptBR ?? country.en
+    }
 }
