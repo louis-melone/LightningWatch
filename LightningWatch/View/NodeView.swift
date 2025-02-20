3bab6f19fa86015c7b8b7b86964d3ffd3cd3fd2f
louis-melone <114934187+louis-melone@users.noreply.github.com>
2025-02-20 09:31:20 -0500

    add medium sheet presentation
    sheet will show alias, public key, location (if any)

--- LightningWatch/View/NodeView.swift
+++ LightningWatch/View/NodeView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct NodeView: View {
     let viewModel: NodeViewModel
     @State var showCapacity = true
+    @State var showSheet = false
     
     var body: some View {
         HStack {
@@ -21,14 +22,16 @@ struct NodeView: View {
             Spacer()
             statisticsView
         }
+        .sheet(isPresented: $showSheet) {
+            LocationSheetView(viewModel: viewModel.locationSheetViewModel)
+                .presentationDetents([.medium])
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            showSheet = true
+        }
     }
 
-    var firstSeenView: some View {
-        Text("\(viewModel.firstSeenFormatted)")
-            .font(.footnote)
-            .foregroundColor(.gray)
-    }
-    
     var footerView: some View {
         HStack {
             TimelineView(.everyMinute) { _ in
