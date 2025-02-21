cda8309b8851154ab8238b2bec4b8c271163df06
louis-melone <114934187+louis-melone@users.noreply.github.com>
2025-02-21 01:34:00 -0500

    Conform CollectionView to LoadableView
    set logic in viewmodel to properly set loadableViewState

--- LightningWatch/ViewModel/CollectionViewModel.swift
+++ LightningWatch/ViewModel/CollectionViewModel.swift
@@ -13,28 +13,8 @@ import SwiftUI
 class CollectionViewModel: ObservableObject {
     @Published var nodeViewModels: [NodeViewModel] = []
     @Published var sortParameter: SortParameter = .channels
+    @Published var loadableViewState: LoadableViewState = .loaded
     var updatedDatetime: Date?
-    
-    func fetchNodes() async {
-        let currentDatetime = Date()
-        if let updatedDatetime, currentDatetime.timeIntervalSince(updatedDatetime) < 1 { return }
-        // TODO: Error Screens
-        let lightningNodes =  try! await ContentFetchManager.fetchLightningNodes()
-        nodeViewModels = lightningNodes
-                            .map { NodeViewModel(node: $0) }
-        sort(by: sortParameter)
-        updatedDatetime = currentDatetime
-    }
-    
-    func sort(by sortParameter: SortParameter) {
-        self.sortParameter = sortParameter
-        switch sortParameter {
-        case .capacity:
-            nodeViewModels.sort(by: { $0.node.capacity > $1.node.capacity })
-        case .channels:
-            nodeViewModels.sort(by: { $0.node.channels > $1.node.channels })
-        }
-    }
     
     var capacityTextColor: Color {
         if sortParameter == .capacity {
@@ -48,6 +28,46 @@ class CollectionViewModel: ObservableObject {
             return Color.black
         }
         return Color.gray
+    }
+    
+    func fetchNodes() async {
+        let currentDatetime = Date()
+        if let updatedDatetime, currentDatetime.timeIntervalSince(updatedDatetime) < 10 { return }
+        if nodeViewModels.isEmpty {
+            loadableViewState = .loading
+        }
+        do {
+            let lightningNodes =  try await Loader.fetchLightningNodes()
+            updatedDatetime = currentDatetime
+            nodeViewModels = lightningNodes.map { NodeViewModel(node: $0) }
+            sort(by: sortParameter)
+            loadableViewState = .loaded
+        } catch {
+            handleError(error: error)
+        }
+    }
+    
+    func sort(by sortParameter: SortParameter) {
+        self.sortParameter = sortParameter
+        switch sortParameter {
+        case .capacity:
+            nodeViewModels.sort(by: { $0.node.capacity > $1.node.capacity })
+        case .channels:
+            nodeViewModels.sort(by: { $0.node.channels > $1.node.channels })
+        }
+    }
+    
+    private func handleError(error: Error) {
+        if let loaderError = error as? LoaderError {
+            switch loaderError {
+            case .noInternet:
+                loadableViewState = .error(.resourceNotFound)
+            default:
+                loadableViewState = .error(loaderError)
+            }
+        } else {
+            loadableViewState = .error(.resourceNotFound)
+        }
     }
 }
 
